4d60bb55adea90ff166c34a2a9429b4375a30fc7
André Lago <andrelago1995@gmail.com>
2018-02-14 16:41:51 +0000

    Initial app version

---
+++ app/app/src/main/java/com/enei/eneiapp/MainActivity.java
@@ -0,0 +1,40 @@
+package com.enei.eneiapp;
+
+import android.os.Build;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = findViewById(R.id.mainwebview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setAppCacheEnabled(true);
+
+        myWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    String url = request.getUrl().toString();
+                    if(url.startsWith("https://enei.pt")) {
+                        view.loadUrl(url);
+                    } else {
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+
+        myWebView.loadUrl("https://enei.pt/");
+    }
+}
