74f3ebe3e3eb11488d693d6dbfb0a2e830c79737
André Lago <andrelago1995@gmail.com>
2018-02-16 18:04:45 +0000

    Slight untrolhafy

--- app/app/src/main/java/com/enei/eneiapp/AppHomeActivity.java
+++ app/app/src/main/java/com/enei/eneiapp/AppHomeActivity.java
@@ -1,20 +1,16 @@
 package com.enei.eneiapp;
 
 import android.content.Intent;
-import android.content.res.AssetManager;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
 
-import java.util.Locale;
+import com.enei.eneiapp.utils.Constants;
 
 public class AppHomeActivity extends AppCompatActivity {
 
@@ -22,28 +18,31 @@ public class AppHomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button button1 = findViewById(R.id.button);
-        Button button2 = findViewById(R.id.button2);
-        Typeface custom_font = Typeface.createFromAsset(getAssets(), "Montserrat-Regular.ttf");
-        button1.setTypeface(custom_font);
-        button2.setTypeface(custom_font);
-        button1.setBackgroundColor(Color.rgb(90, 0, 255));
-        button2.setBackgroundColor(Color.rgb(90, 0, 255));
+        Button websiteButton = findViewById(R.id.button_website);
+        Button programButton = findViewById(R.id.button_program);
 
-        button1.setOnClickListener(new View.OnClickListener() {
+        Typeface customFont = Typeface.createFromAsset(getAssets(), Constants.FONT_PATH);
+        websiteButton.setTypeface(customFont);
+        programButton.setTypeface(customFont);
+        websiteButton.setBackgroundColor(Constants.BACKGROUND_PURPLE);
+        programButton.setBackgroundColor(Constants.BACKGROUND_PURPLE);
+
+        websiteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://enei.pt"));
+                // Open website in browser
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(Constants.ENEI_WEBSITE));
                 startActivity(browserIntent);
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        programButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Open program activity
                 Intent myIntent =
                         new Intent(AppHomeActivity.this, ScheduleActivity.class);
                 AppHomeActivity.this.startActivity(myIntent);
---
+++ app/app/src/main/java/com/enei/eneiapp/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.enei.eneiapp.utils;
+
+import android.graphics.Color;
+
+/**
+ * Created by Lago on 16/02/2018.
+ */
+
+public abstract class Constants {
+    public static final String ENEI_WEBSITE = "https://enei.pt";
+    public static final String FONT_PATH = "Montserrat-Regular.ttf";
+
+    public static final int BACKGROUND_PURPLE = Color.rgb(90, 0, 255);
+}
